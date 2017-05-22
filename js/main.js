195bd83bc91880d5ee68d8c3323255e509160e9e
Mohd Khairi Mohd Adnan <khairi@labs.my>
2017-05-22 19:05:13 +0800

    simplify pages

--- js/main.js
+++ js/main.js
@@ -1,49 +1,75 @@
 var endpointUrl = 'http://api.solat.my/zones';
-
+Vue.config.devtools = true
 var Zon = Vue.resource(endpointUrl + '.json');
 var Calendar = Vue.resource(endpointUrl + '{/zone}.json');
 //var Calendar = Vue.resource(endpointUrl + 'year{/yy}/month{/mm}/locations{/zone}.json');
 
-var vm = new Vue({
-  el: '#app',
-  data: {
+var data =  {
     zones: [],
-    current_zone: "JHR01",
+    zone: "JHR01",
+    year: 2017,
+    month: 05,
     days: [],
-    year: '',
-    month: ''
-  },
+    today: [],
+  }
+
+var daily = new Vue({
+  el: '#daily',
+  data: data,
+  methods: {
+    fetchData: function(yy,mm,zone) {
+    self = this
+    Calendar.get({zone: zone}).then(function (response) {
+        self.today = response.data;
+      });
+    }
+  }
+})
+
+var monthly = new Vue({
+  el: '#monthly',
+  data: data,
+  methods: {
+    fetchData: function(yy, mm, zone) {
+      self = this
+      Calendar.get({year: yy, month: mm, zone: zone}).then(function (response) {    
+        self.days = response.data;
+      });
+    },
+    
+    getThisMonth: function(mm) {
+      this.month = mm
+      self.fetchData(this.year, mm, this.zone);
+    }
+  }
+})
+
+var locations = new Vue({
+  el: '#locations',
+  data: data,
   computed: {
 
   },
   mounted: function() {
-	  this.fetchZones();
-    this.year = 2017;
-    this.month = 5;
-    this.fetchData(this.year, this.month, this.current_zone);
+    this.fetchZones();
+    daily.fetchData(this.year, this.month, this.zone);
+    monthly.fetchData(this.year, this.month, this.zone);
   },
+
   methods: {
     fetchZones: function() {
-	  self = this
+      self = this
       Zon.get({}).then(function (response) {
         self.zones = response.data
       });
     },
     
-    fetchData: function(yy,mm,zone) {
-	  self = this
-	  Calendar.get({zone: zone}).then(function (response) {
-        self.days = response.data;
-      });
-    },
-    
     getThisZone: function(kod) {
-      this.fetchData(2017, this.month, kod);
-      this.current_zone = kod;
+      this.zone = kod;
+      monthly.fetchData(this.year, this.month, this.zone);
     },
-
-    getThisMonth: function(mm) {
-      this.fetchData(2017, mm, 'jhr01');
-    }
   }
 })
+
+
+
