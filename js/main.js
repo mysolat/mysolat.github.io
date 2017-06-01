6e9b21070214fad33b36c05864a633a581752e42
Mohd Khairi Mohd Adnan <khairi@labs.my>
2017-06-01 17:19:53 +0800

    update btn

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function getCookie(cname) {
   var name = cname + "=";
   var decodedCookie = decodeURIComponent(document.cookie);
   var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
+  for(var i = 0; i < ca.length; i++) {
       var c = ca[i];
       while (c.charAt(0) == ' ') {
           c = c.substring(1);
@@ -196,6 +196,7 @@ var settings = new Vue({
     }
 
   },
+
   mounted: function() {
     this.init();
   },
